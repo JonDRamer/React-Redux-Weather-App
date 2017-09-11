623baaf506bf1b0575771b1daf8f3f6c52b8b4ab
Jon Ramer <jramer87@gmail.com>
2017-09-11 17:05:01 -0500

    Connects FETCH_WEATHER action with onFormSubmit event

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '9edefc5b7b941f82c11b1b64cf6174d5';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
