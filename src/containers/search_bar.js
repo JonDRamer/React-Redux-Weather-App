45aac8551dd7c36fbe71bb7b0a9af51a317b746b
Jon Ramer <jramer87@gmail.com>
2017-09-11 16:45:50 -0500

    Adds FETCH_WEATHER action

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,4 +1,4 @@
- import React, { Component } from 'react';
+  import React, { Component } from 'react';
 
  export default class SearchBar extends Component {
    constructor(props) {
@@ -10,13 +10,16 @@
    }
 
    onInputChange(event) {
-     console.log(event.target.value);
      this.setState({ term: event.target.value });
+   }
+
+   onFormSubmit(event) {
+      event.preventDefault();
    }
 
    render() {
      return (
-        <form className="input-group">
+        <form onSubmit={this.onFormSubmit} className="input-group">
           <input
             placeholder="Get a five-day forecast in your favorite cities"
             className="form-control"
